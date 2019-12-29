1cb5c3e3ca02f4d646e5864afdf60265bbeac882
Matt Newman <mdjnewman@gmail.com>
2019-12-29 11:12:25 -0600

    Script to backup edge preferences

---
+++ bin/backup-edge.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cd ~ || exit
+
+OUT="$(tar --create \
+    --gzip \
+    --file "./Dropbox/Backups/Edge/$(gdate --universal --iso-8601=seconds | sed -e 's/\://g').tar.gz" \
+    '/Volumes/GARMIN/Garmin/Courses' \
+    '/Volumes/GARMIN/Garmin/Locations' \
+    '/Volumes/GARMIN/Garmin/Records' \
+    '/Volumes/GARMIN/Garmin/Settings' \
+    '/Volumes/GARMIN/Garmin/Sports' \
+    '/Volumes/GARMIN/Garmin/Totals' \
+    '/Volumes/GARMIN/Garmin/Weight' \
+    '/Volumes/GARMIN/Garmin/Workouts'
+    2>&1)"
+
+EXIT_CODE=$?
+
+if [[ $EXIT_CODE != 0 || "$OUT" == *"ERROR"* ]]; then
+    echo Exit code is $EXIT_CODE
+    echo "$OUT"
+fi
+
+exit $EXIT_CODE
