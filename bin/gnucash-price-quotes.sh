1bfb4c329c3800fc6f278efea8bd875ca60f53ee
Matt Newman <mdjnewman@gmail.com>
2021-09-27 10:13:46 +1000

    Fix gnucash-price-quotes.sh

--- bin/gnucash-price-quotes.sh
+++ bin/gnucash-price-quotes.sh
@@ -1,13 +1,15 @@
 #!/usr/local/bin/bash
 
+export PATH=/usr/local/bin:$PATH
+
 # Not strictly, but my .bash_profile does require it
 if ((BASH_VERSINFO[0] < 4)); then echo "Sorry, you need at least bash-4.0 to run this script." >&2; exit 1; fi
 
-. /Users/mnewman/.bash_profile > /dev/null
+export HOME=/Users/mnewman
 
-export PATH=/usr/bin:$PATH
+. "$HOME/.bash_profile" > /dev/null
 
-OUT="$(/Applications/Gnucash.app/Contents/MacOS/Gnucash --debug --add-price-quotes='/Users/mnewman/Google Drive (mdjnewman@gmail.com)/Money/GNUCash Files/Money.gnucash' 2>&1)"
+OUT="$(/Applications/Gnucash.app/Contents/MacOS/gnucash-cli --debug --quotes get '/Volumes/GoogleDrive-110379146933208071142/My Drive/Money/GNUCash Files/Money.gnucash' 2>&1)"
 EXIT_CODE=$?
 
 if [[ $EXIT_CODE != 0 || "$OUT" == *"ERROR"* ]]; then
